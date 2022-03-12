0007e287c491affe01b26548391b82b71f9ff6d8
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2022-03-11 23:13:02 -0700

    Creating Mission 10

---
+++ Mission 10/Mission7/Models/EFBookRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Mission7.Models;
+using Mission7.Models.ViewModels;
+
+namespace Mission7.Models
+{
+    public class EFBookRepository : IBookRepository
+    {
+        private BookContext context { get; set; }
+
+        public EFBookRepository(BookContext temp)
+        {
+            context = temp;
+        }
+
+
+
+        public IQueryable<Book> Books => context.Books;
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
+
+    }
+}
+
+
